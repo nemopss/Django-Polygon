37c7f183bd9bd1b9c5c6033d9c34ecb4516f88e6
Libra <elizavetakocheva0310@mail.ru>
2024-03-11 13:47:25 +0300

    changes_learn

--- papapa/poligon/polygon/urls.py
+++ papapa/poligon/polygon/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='main'), # http://127.0.0.1:8000/
-    path('learn', learn, name='learn'), # http://127.0.0.1:8000/learn
+    path('learn/', learn, name='learn'), # http://127.0.0.1:8000/learn
     path('registry/', RegisterUser.as_view(), name='registry'), # http://127.0.0.1:8000/registry
     path('auth', auth, name='auth'), # http://127.0.0.1:8000/learn
 
